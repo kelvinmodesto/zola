605381e645f70a47b3759890e61bca03c3b6d9da
Kelvin Modesto <km.modesto@gmail.com>
2019-06-19 01:01:18 -0300

    Add symbol controller

---
+++ src/controllers/financial/symbolController.ts
@@ -0,0 +1,11 @@
+export default class SymbolController {
+  public router: any;
+  public context: any;
+  constructor(router: any, context: any) {
+    this.router = router;
+    this.context = context;
+  }
+  public init() {
+    // list all routes
+  }
+}
